71df0dfc0afd81d02bd1c21ec1f56a41ea2b5c1a
Steve Robinett <steven.robinett@gfcmsu.edu>
2018-03-28 22:40:33 -0600

    add toProduction.sh

--- deploy/toProduction.sh
+++ deploy/toProduction.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+# this script will be responsible for copying the entire project
+#from a local repo to the target platfoem on steverobinett.com
+
+#ToDo:
+#   set up sub-domain and establish sftp credentials
+#   decide on operational procedures
