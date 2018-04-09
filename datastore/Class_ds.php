3e5b1520361699be1821acb623f956155e7067c2
Donnie Harvell <31457792+creativetech1@users.noreply.github.com>
2018-04-08 22:40:27 -0600

    Updated Class_ds.php to include Select functions.

--- datastore/Class_ds.php
+++ datastore/Class_ds.php
@@ -14,7 +14,23 @@
       $qry = "INSERT INTO classes (classID, className, classSection, classSemester, classInstructor, classScheduleMonStart, classScheduleMonEnd, classScheduleTuesStart, classScheduleTuesEnd, classScheduleWedStart, classScheduleWedEnd, classScheduleThursStart, classScheduleThursEnd, classScheduleFriStart, classScheduleFriEnd) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
       if($stmt = $conn->prepare($qry)){
-        $stmt->bind_param('sssssssssssssss', $this->classID, $this->className, $this->classSection, $this->classSemester,$this->classInstructor, $this->classScheduleMonStart, $this->classScheduleMonEnd, $this->classScheduleTuesStart, $this->classScheduleTuesEnd, $this->classScheduleWedStart, $this->classScheduleWedEnd,               $this->classScheduleThursStart, $this->classScheduleThursEnd, $this->classScheduleFriStart,     $this->classScheduleFriEnd);
+        $stmt->bind_param('sssssssssssssss',
+        $this->classID,
+        $this->className,
+        $this->classSection,
+        $this->classSemester,
+        $this->classInstructor,
+        $this->classScheduleMonStart,
+        $this->classScheduleMonEnd,
+        $this->classScheduleTuesStart,
+        $this->classScheduleTuesEnd,
+        $this->classScheduleWedStart,
+        $this->classScheduleWedEnd,
+        $this->classScheduleThursStart,
+        $this->classScheduleThursEnd,
+        $this->classScheduleFriStart,
+        $this->classScheduleFriEnd
+      );
 
         $this->classID = $conn->real_escape_string($arr['classID']);
         $this->className = $conn->real_escape_string($arr['className']);
@@ -33,15 +49,124 @@
         $this->classScheduleFriEnd = $conn->real_escape_string($arr['classScheduleFriEnd']);
       
 
-        if($stmt->execute()){
-          echo "Records inserted successfully.";
-        } else{
-            echo "ERROR: Could not execute query: $sql. " . $conn->error;
-          }
-      } else{
-          echo "ERROR: Could not prepare query: $sql. " . $conn->error;
-        }
+        
+      }
+    }
 
+
+    
+    
+    
+    
+    
+    public function selectSingle($key){
+      $qry = 'SELECT * FROM Classes WHERE classID = ?';
+      
+      $stmt = $this->conn->prepare($qry);
+      $stmt->bind_param('s', $key);
+      $stmt->execute();
+      
+      $stmt->bind_result(
+        $this->classID,
+        $this->className,
+        $this->classSection,
+        $this->classSemester,
+        $this->classInstructor,
+        $this->classScheduleMonStart,
+        $this->classScheduleMonEnd,
+        $this->classScheduleTuesStart,
+        $this->classScheduleTuesEnd,
+        $this->classScheduleWedStart,
+        $this->classScheduleWedEnd,
+        $this->classScheduleThursStart,
+        $this->classScheduleThursEnd,
+        $this->classScheduleFriStart,
+        $this->classScheduleFriEnd
+      );
+
+      $row = array();
+      while ($stmt->fetch()) {
+        array_push($row, $this->classID);
+        array_push($row, $this->className);
+        array_push($row, $this->classSection);
+        array_push($row, $this->classSemester);
+        array_push($row, $this->classInstructor);
+        array_push($row, $this->classScheduleMonStart);
+        array_push($row, $this->classScheduleMonEnd);
+        array_push($row, $this->classScheduleTuesStart);
+        array_push($row, $this->classScheduleTuesEnd);
+        array_push($row, $this->classScheduleWedStart);
+        array_push($row, $this->classScheduleWedEnd);
+        array_push($row, $this->classScheduleThursStart);
+        array_push($row, $this->classScheduleThursEnd);
+        array_push($row, $this->classScheduleFriStart);
+        array_push($row, $this->classScheduleFriEnd);
+      }
+      if (!empty($row)) {
+          return $row;
+      } else {
+          return null;
+      }
+    }
+
+
+
+
+
+    public function selectAll($sel_all){
+      $sel_all = '*';
+      $qry = 'SELECT '. $sel_all.' FROM Classes';
+      $stmt = $this->conn->prepare($qry);
+      $stmt->execute();
+      $stmt->store_result();
+      $stmt->bind_result(
+        $this->classID,
+        $this->className,
+        $this->classSection,
+        $this->classSemester,
+        $this->classInstructor,
+        $this->classScheduleMonStart,
+        $this->classScheduleMonEnd,
+        $this->classScheduleTuesStart,
+        $this->classScheduleTuesEnd,
+        $this->classScheduleWedStart,
+        $this->classScheduleWedEnd,
+        $this->classScheduleThursStart,
+        $this->classScheduleThursEnd,
+        $this->classScheduleFriStart,
+        $this->classScheduleFriEnd
+      );
+
+      $returnSet = array();
+      $rowCount = 0;
+      while ($stmt->fetch()) {
+        $row = array();
+
+        array_push($row, $this->classID);
+        array_push($row, $this->className);
+        array_push($row, $this->classSection);
+        array_push($row, $this->classSemester);
+        array_push($row, $this->classInstructor);
+        array_push($row, $this->classScheduleMonStart);
+        array_push($row, $this->classScheduleMonEnd);
+        array_push($row, $this->classScheduleTuesStart);
+        array_push($row, $this->classScheduleTuesEnd);
+        array_push($row, $this->classScheduleWedStart);
+        array_push($row, $this->classScheduleWedEnd);
+        array_push($row, $this->classScheduleThursStart);
+        array_push($row, $this->classScheduleThursEnd);
+        array_push($row, $this->classScheduleFriStart);
+        array_push($row, $this->classScheduleFriEnd);
+
+        $rowCount++;
+
+        array_push($returnSet, $row);
+      }
+      if ($rowCount > 0) {
+          return $returnSet;
+      } else {
+          return null;
+      }
+    }
   }
-}
 ?>
