9747f10df00f9bfe6299e9a4ae3932856f3a4d1f
Wayne Breau <wbreau@gmail.com>
2018-04-15 22:43:29 -0600

    Committing User.php and User_ds.php

---
+++ data/User.php
@@ -0,0 +1,13 @@
+<?php
+class Users { 
+
+   var $userID;
+   var $userLogin;
+   var $userPass;
+   var $userFName;
+   var $userLName;
+   var $roleID;
+}
+
+
+ ?>
---
+++ datastore/User_ds.php
@@ -0,0 +1,99 @@
+<?php
+  require('../data/User.php');
+
+  class User_ds extends Users{
+
+    public $conn;
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function insert($arr){
+
+      $qry = "INSERT INTO users (userID, userLogin, userPass, userFName, userLName, roleID) VALUES (?, ?, ?, ?, ?, ?)";
+
+      if($stmt = $conn->prepare($qry)){
+        $stmt->bind_param('ssssss',
+        $this->userID,
+        $this->userLogin,
+        $this->userPass,
+        $this->userFName,
+        $this->userLName, 
+        $this->roleID
+        );
+
+        $this->userID = $conn->real_escape_string($arr['userID']);
+        $this->userLogin = $conn->real_escape_string($arr['userLogin']);
+        $this->userPass = $conn->real_escape_string($arr['userPass']);
+        $this->userFName = $conn->real_escape_string($arr['userFName']);
+        $this->userLName = $conn->real_escape_string($arr['userLName']);
+        $this->roleID = $conn->real_escape_string($arr['roleID']);
+        
+      }
+    }
+
+
+
+
+
+
+
+    public function selectSingle($key){
+      $qry = 'SELECT * FROM Users WHERE userID = ?';
+
+      $stmt = $this->conn->prepare($qry);
+      $stmt->bind_param('s', $key);
+      $stmt->execute();
+
+      $stmt->bind_result(
+        $this->userID,
+        $this->userName
+      );
+
+      $row = array();
+      while ($stmt->fetch()) {
+        array_push($row, $this->userID);
+        array_push($row, $this->userName);
+      }
+      if (!empty($row)) {
+          return $row;
+      } else {
+          return null;
+      }
+    }
+
+
+
+
+
+    public function selectAll(){
+      $qry = 'SELECT * FROM Users';
+      $stmt = $this->conn->prepare($qry);
+      $stmt->execute();
+      $stmt->store_result();
+      $stmt->bind_result(
+        $this->userID,
+        $this->userName
+      );
+
+      $returnSet = array();
+      $rowCount = 0;
+      while ($stmt->fetch()) {
+        $row = array();
+
+        array_push($row, $this->userID);
+        array_push($row, $this->userName);
+
+        $rowCount++;
+
+        array_push($returnSet, $row);
+      }
+      if ($rowCount > 0) {
+          return $returnSet;
+      } else {
+          return null;
+      }
+    }
+  }
+?>
